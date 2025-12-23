d76574cfcfafd02fc42bb546c040431e2d654aca
hawra <hawra.yusuf02@outlook.com>
2025-12-23 16:27:03 +0300

    imported product controller into server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const PORT = process.env.PORT || 3000;
 const testJwtRouter = require('./controllers/test-jwt');
 const authCtrl = require('./controllers/auth');
 const usersCtrl = require('./controllers/users');
+const productsCtrl = require ('./controllers/product');
 
 // MiddleWare
 const verifyToken = require('./middleware/verify-token');
@@ -35,6 +36,7 @@ app.use('/test-jwt', testJwtRouter);
 // Protected Routes
 app.use(verifyToken);
 app.use('/users', usersCtrl);
+app.use('/products', productsCtrl);
 
 app.listen(PORT, () => {
   console.log('The express app is ready!');
